364524243d01af034c4620d82c163f22da395685
dim-dev <dim.dev@outlook.com>
2018-10-16 11:36:20 +0200

    Static resources. A simple css stylesheets added.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const controllers = require('./controllers');
 
 const app = express();
 app.use(morgan('tiny'));
 app.set('view engine', 'vash');
+app.use(express.static(path.join(__dirname, '/public')));
 controllers.init(app);
 
 app.get('/api/users', (req, res) => {
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,7 +4,7 @@ const data = require('../data');
 exports.init = (app) => {
   app.get('/', (req, res) => {
     data.getNoteCategories((err, results) => {
-      res.render('index', { title: 'my title', error: err, categories: results });
+      res.render('index', { title: 'Library', error: err, categories: results });
     });
   });
 };
